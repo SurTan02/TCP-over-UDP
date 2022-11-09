19890632dc2d6c34ffd38ff38c4ba98300d9e0c5
Suryanto Tan <suryanto.tan5@gmail.com>
2022-11-09 21:45:13 +0700

    add file transfer

--- client.py
+++ client.py
@@ -1,26 +1,41 @@
 from lib.connection import SocketConnection, Connection
-from lib.segment import Segment, SegmentHeader
+from lib.segment import Segment, SegmentHeader, SegmentFlag
 from typing import Tuple, List, Dict
-
+import socket
 
 class Client:
     socket: SocketConnection
     addr: Tuple[str, int]
     connection: Connection
 
-    def __init__(self, ip: str, port: int):
+    def __init__(self, ip: str, port: int, path : str):
         self.socket = SocketConnection(ip, port)
         self.connection = None
         self.addr = None
+        self.path = path
 
     def listen(self):
         seg, addr = self.socket.listen()
         is_valid = seg.valid_checksum()
-
+        
         if is_valid:
             self._handle_connection(seg, addr)
         else:  # Gatau mo ngapain ini, seharusnya buang aja
             print("Invalid checksum from", addr)
+
+    def _send_ack(self, ack_num : int, seq_num : int = 0):
+        if ack_num >= 0:
+
+            ack_resp = Segment(
+                header={
+                "seq_num": seq_num,
+                "ack_num": ack_num,
+                "flag": SegmentFlag.get_flag("ACK"),
+                "checksum": None
+            },
+                payload=b""
+            )
+            self.socket.send(ack_resp, self.addr)
 
     def _handle_connection(self, seg: Segment, addr: Tuple[str, int]):
         if self.addr is None:
@@ -41,11 +56,13 @@ class Client:
                 self.connection['headers'].append(header)
                 self.socket.send(Segment.SYN_ACK(), addr)
                 self.connection['state'] = "SYN_SENT"
+
         elif state == "SYN_RCVD":
             if header['flag'] == "ACK":
                 self.connection['headers'].append(header)
                 self.connection['state'] = "ESTABLISHED"
                 print("SocketConnection established with", addr)
+
         elif state == "SYN_SENT":
             if header['flag'] == "SYN-ACK":
                 self.connection['headers'].append(header)
@@ -58,6 +75,7 @@ class Client:
     def connect(self, ip: str, port: int):
         self.socket.send(Segment.SYN(), (ip, port))
         self.addr = (ip, port)
+        
         self.connection = {
             'addr': self.addr,
             'headers': [],
@@ -66,8 +84,49 @@ class Client:
         }
         self.listen()
 
+    def listen_file_transfer(self):
+        print(f"[!] Receiving file content...")
+
+        with open(self.path, "wb") as dstFile:
+            Rn = 0
+            while True:
+                try:
+                    seg, addr = self.socket.listen()
+                    is_valid = seg.valid_checksum()
+
+                    if is_valid and addr == self.addr:
+                        Sn = seg.get_header()["seq_num"]
+                        if Rn == Sn:
+                            print(f"[!] Sn={Rn} Received, sending ACK {Rn}")
+
+                            dstFile.write(seg.get_payload().rstrip(b'\x00'))
+                            
+
+                            # Send acknowledgement for last received segment
+                            self._send_ack(Rn)
+                            Rn += 1
+
+                        # End of file
+                        elif seg.get_flag() == "FIN":
+                            print(f"[!] Successfully received file")
+                            
+                            # Send acknowledgement for last received segment
+                            self.socket.send(seg.FIN_ACK(), self.addr)
+                            break
+                        else:
+                            # Refuse segment
+                            print(f"[!] Sn not equal with Rn ({Sn} =/= {Rn}), ignoring...")
+                    else:
+                        print(f'[!] Checksum failed')
+                except socket.timeout:
+                    print(f"[!] timeout, resending ACK {Rn-1}...")
+                    self._send_ack(Rn)
 
 if __name__ == '__main__':
-    main = Client("127.0.0.1", 7001)
+    main = Client("127.0.0.1", 7001, "./dump/tes.txt")
     main.connect("127.0.0.1", 7000)
-    print(main.connection)
+
+    main.listen()
+
+    main.listen_file_transfer()
+    # print(main.connection)
--- server.py
+++ server.py
@@ -1,18 +1,29 @@
-from lib.segment import Segment, SegmentHeader
+from lib.segment import Segment, SegmentHeader, SegmentFlag
 from lib.connection import SocketConnection, Connection
 from typing import Tuple, List, Dict
+import math, socket
 
+SEGMENT_SIZE = 32768
 
 class Server:
     socket: SocketConnection
     connections: Dict[Tuple[str, int], Connection]
 
-    def __init__(self, ip: str, port: int):
+    def __init__(self, ip: str, port: int, path : str):
         self.socket = SocketConnection(ip, port)
         self.connections = {}
+        self.path = path
+        
+        with open(self.path, "rb") as srcFile:
+            srcFile.seek(0, 2)
+            self.filesize = srcFile.tell()  
+        
+        self.segmentCount = math.ceil(self.filesize / SEGMENT_SIZE)
 
     def listen(self):
         seg, addr = self.socket.listen()
+        # print(seg, addr)
+
         is_valid = seg.valid_checksum()
         if is_valid:
             self._handle_connection(seg, addr)
@@ -51,9 +62,81 @@ class Server:
 
         # Tinggal tambahin case aja berdasarkan state sama flag
 
+    def start_file_transfer(self):
+        # Handshake & file transfer for all client
+        pass
+
+
+
+    # def file_transfer(self, client_addr : ("ip", "port")):
+    def file_transfer(self):
+        # File transfer, server-side, Send file to 1 client
+
+        Sb = 0                                      # Seq_Base
+        N = 4                                       # Ukuran Window
+        Sw = min(Sb + N, self.segmentCount)         # Sliding Window
+        Sm = Sw + 1                                 # Sequence Max
+
+        seg, addr = self.socket.listen()
+        with open(self.path, 'rb') as srcFile:
+            print(f"[!] Sending file content...")
+            
+            while Sb < self.segmentCount:
+                try:
+                    for i in range(Sw - Sb):
+                        
+                        srcFile.seek(SEGMENT_SIZE * (Sb + i))
+                        data = Segment(header={
+                            "seq_num" : Sb+i, 
+                            "ack_num" : 0,
+                            "flag" : SegmentFlag.get_flag("ACK"),
+                            "checksum" : None
+                            }, 
+                            payload = srcFile.read(SEGMENT_SIZE)
+                            )
+                        self.socket.send(data, addr)
+                        print(f"[!] Sending segment with Sn {Sb + i}")
+                    
+                    Sm = Sw - Sb
+                    while Sb < Sm:
+                        seg, addr = self.socket.listen()
+                        isValid = seg.valid_checksum()
+
+                        if isValid and seg.get_flag() == "ACK":
+                            Rn = seg.get_header()["ack_num"]
+                            if (Rn == Sb):
+                                Sb += 1
+                                Sw += 1
+                                print(f"[!] Received ACK {Rn}")
+                                
+                            elif Rn > Sb:
+                                print(f"[!] Received ACK {Rn}")
+                                # Sm = Sm - Sb + Rn
+                                Sb = Rn
+                                
+                            else:
+                                print(f"[!] ACK number {Rn} < Sb ({Sb}), ignoring...")
+                        elif not isValid:
+                            print(f"[!] Checksum failed")
+                            break
+                        else:
+                            print(f"[!] Unknown error")
+                            break
+                except socket.timeout:
+                    print(f"[!] timeout")
+                    
+        print(f"[!] Successfully sent file to {addr[0]}:{addr[1]}")
+        self.socket.send(Segment.FIN(), addr)
 
 if __name__ == '__main__':
-    server = Server("127.0.0.1", 7000)
+    server = Server("127.0.0.1", 7000, "./dump/bro.txt")
+    print(f"[!] Server started at 127.0.0.1:7000...")
     while True:
+
+        # Handshake
         server.listen()
-        print(server.connections)
+
+        # File Transfer
+        server.file_transfer()
+        break
+        # print(server.connections)
